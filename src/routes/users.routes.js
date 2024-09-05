0ed1b3f20a942ef2298e6db1771025506f0a3720
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-09-05 15:55:50 -0300

    Improvement: Create Avatar endpoint

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,20 @@
 const { Router } = require("express");
+const multer = require("multer");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
+const uploadConfig = require("../configs/upload");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+    console.log(request.file.filename)
+    return response.json();
+});
 
 module.exports = usersRoutes;
