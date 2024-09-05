b0081a980f813de41e037bb20f40a7bdc3d6bf58
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-09-04 22:15:00 -0300

    Add: Email/Password verification in API

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -5,7 +5,7 @@ class NotesController {
         const { title, description, rating, tags } = request.body;
         const { user_id } = request.params;
 
-        const [note_id] = await knex("movie_notes").insert({
+        const [note_id] = await knex("notes").insert({
             title,
             description,
             rating,
@@ -20,7 +20,7 @@ class NotesController {
             }
         });
 
-        await knex("movie_tags").insert(tagsInsert);
+        await knex("tags").insert(tagsInsert);
 
         return response.status(201).json();
     }
@@ -28,8 +28,8 @@ class NotesController {
     async show(request, response) {
         const { id } = request.params;
 
-        const note = await knex("movie_notes").where({ id }).first();
-        const tags = await knex("movie_tags").where({ note_id: id }).orderBy("id");
+        const note = await knex("notes").where({ id }).first();
+        const tags = await knex("tags").where({ note_id: id }).orderBy("id");
 
         return response.json({
             ...note,
@@ -40,7 +40,7 @@ class NotesController {
     async delete(request, response) {
         const { id } = request.params;
 
-        await knex("movie_notes").where({ id }).delete();
+        await knex("notes").where({ id }).delete();
 
         return response.json({
             message: "Nota deletada com sucesso"
@@ -55,29 +55,29 @@ class NotesController {
         if (tags) {
             const filterTags = tags.split(',').map(tag => tag.trim());
 
-            notes = await knex("movie_tags")
+            notes = await knex("tags")
                 .select([
-                    "movie_notes.id",
-                    "movie_notes.title",
-                    "movie_notes.user_id"
+                    "notes.id",
+                    "notes.title",
+                    "notes.user_id"
                 ])
-                .where("movie_notes.user_id", user_id)
-                .whereLike("movie_notes.title", `%${title}%`)
+                .where("notes.user_id", user_id)
+                .whereLike("notes.title", `%${title}%`)
                 .whereIn("name", filterTags)
-                .innerJoin("movie_notes", "movie_notes.id", "movie_tags.note_id")
-                .orderBy("movie_notes.title");
+                .innerJoin("notes", "notes.id", "tags.note_id")
+                .orderBy("notes.title");
         } else if (title) {
-            notes = await knex("movie_notes")
+            notes = await knex("notes")
                 .where({ user_id })
                 .whereLike("title", `%${title}%`)
                 .orderBy("title");
         } else {
-            notes = await knex("movie_notes")
+            notes = await knex("notes")
                 .where({ user_id })
                 .orderBy("title");
         }
 
-        const userTags = await knex("movie_tags").where({ user_id });
+        const userTags = await knex("tags").where({ user_id });
         const notesWithTags = notes.map(note => {
             const noteTags = userTags.filter(tag => tag.note_id === note.id);
 
