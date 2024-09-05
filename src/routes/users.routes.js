7075e3b7bb0ad5045a0f4a73ae9ba2931f8a97bf
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-09-05 16:54:52 -0300

    Improvement: Create UserAvatarController

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,19 +2,19 @@ const { Router } = require("express");
 const multer = require("multer");
 
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
+
 const uploadConfig = require("../configs/upload");
 
 const usersRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
-    console.log(request.file.filename)
-    return response.json();
-});
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRoutes;
